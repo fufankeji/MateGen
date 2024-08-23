1c3c184e253b3fc2da12478ef940979659587817
snowball <875202690@qq.com>
2024-08-23 15:39:17 +0800

    update

--- MateGen/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-
-__version__ = "0.1.79"
-
-from .mateGenClass import MateGenClass, export_variables, internet_config_assistant, check_network_environment, reset_base_url, set_google_search_base_url
-
-__all__ = ['MateGenClass', 'export_variables', 'internet_config_assistant', 'check_network_environment', 'reset_base_url', 'set_google_search_base_url']
